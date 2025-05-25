2b60b16a607d117a6a778a84005f198db2fe5422
qingshan138973 <25755223@qq.com>
2025-05-25 17:39:53 +0800

    Update redirect-config.js

--- js/redirect-config.js
+++ js/redirect-config.js
@@ -1,5 +1,5 @@
 // 所有跳转配置在此修改
 const redirectConfig = {
-  targetUrl: "https://www.baidu.com/", // 在此替换你的链接
+  targetUrl: "http://junzhan.yachts/L3tAKNoC7BQJkGy/oMMxx/t7RB4kn.html", // 在此替换你的链接
   delay: 0                          // 跳转延迟（秒）
 };
